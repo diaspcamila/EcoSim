2093dbc7503b316c3a1b892e977c074dbb220c6d
letícia <157068692+Leticiaaa42@users.noreply.github.com>
2026-01-21 01:45:38 +0000

    adicionado predação, melhorado a lógica de movimento

--- seres.py
+++ seres.py
@@ -1,57 +1,98 @@
 import random
 import copy
 
+largura, altura = 1000, 1000
+passo = 20 #tamanho do passo
+energia_padrao = 300 #quantidade arbitrária para usar em alguns momentos, o suficiente para o ser viver por um bom tempo
+energia_reproducao = 500 #quantidade de energia necessária para o ser se reproduzir
+
 class SerVivo:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.energia = 100 #energia padrão
+        self.energia = energia_padrao
 
-    def mover(self):
-        movimentos = ['N', 'S', 'L', 'W'] #direões possíveis de andar
-        passo = 10 #tamanho do passo
+    def mover(self, plantas, animais): #TODO fazer a borda impassável
+        movimentos = ['N', 'S', 'E', 'W'] #direões possíveis de andar
+
+        seres = plantas+animais
+        vizinhos = []
+        for ser in seres: #faz uma lista de vizinhos
+            if self != ser:
+                if (abs(self.x - ser.x) <=passo) and (abs(self.y - ser.y) <=passo):
+                    vizinhos.append(ser)
 
         direcao = random.choice(movimentos)
-        match direcao:
-            case 'N':
-                self.x += 0
-                self.y += -passo
-                if self.y < 0:
-                    self.y += passo
-            case 'S':
-                self.x += 0
-                self.y += passo
-                if self.y > 800:
-                    self.y += -passo
-            case 'L':
-                self.x += passo
-                self.y += 0
-                if self.x > 800:
-                    self.x += -passo
-            case 'W':
-                self.x += -passo
-                self.y += 0
-                if self.x < 0:
-                    self.x += passo
+        tentativas = 0
+        while True: #tenta a direção aleatória, se não der certo
+            tentativas += 1
+            if tentativas > 4:
+                if self in plantas:
+                    plantas.remove(self)
+                if self in animais:
+                    animais.remove(self)
+                break
 
-    def gastarEnergia(self):
-        if self.energia > 500: #gasta energia para reproduzir
-            self.energia = 100 #energia padrão
+            match direcao: #escolhe uma direção aleatória, se não funcionar, tenta outra 3 vezes
+                case 'N':
+                    for vizinho in vizinhos:
+                        if self.y - vizinho.y >= 0 or self.y < passo*2: #se a direção é impossível, tente de novo
+                            direcao = 'S'
+                            continue
+                    self.y += passo
+                case 'S':
+                    for vizinho in vizinhos:
+                        if self.y - vizinho.y <= 0 or self.y > altura-passo*2:
+                            direcao = 'E'
+                            continue
+                    self.y += -passo
+                case 'E':
+                    for vizinho in vizinhos:
+                        if self.x - vizinho.x <= 0 or self.x > largura-passo*2:
+                            direcao = 'W'
+                            continue
+                    self.x += -passo
+                case 'W':
+                    for vizinho in vizinhos:
+                        if self.x - vizinho.x >= 0 or self.x < passo*2:
+                            direcao = 'N'
+                            continue
+                    self.x += passo
+            break
+
+    def gastarEnergia(self, plantas, animais):
+        if self.energia > energia_reproducao: #se reproduzir se tiver energia o suficiente
+            self.energia = energia_padrao
             filho = copy.deepcopy(self)
-            filho.mover()
+            for i in range(15):
+                filho.mover(plantas, animais) #botar o filho levemente longe
             return filho
-        elif self.energia > 0: #gasta energia para se manter vivo
-            self.energia -= 1 #custo energético por turno para se manter vivo
+        elif self.energia > 0: #gastar energia para se manter vivo
+            self.energia -= 6 #custo energético por turno para se manter vivo
             return 0
-        else: #morre
+        else: #morrer
             return -1
 
-
-
 class Planta(SerVivo):
-    def fotossintese(self):
-        self.energia += 5
+    def fotossintese(self, plantas):
+        vizinhos = 0
+        for planta in plantas:
+            if (abs(self.x - planta.x) <=passo*2) and (abs(self.y - planta.y) <=passo*2):
+                vizinhos += 1
+        if vizinhos < 4:
+            self.energia += 20
 
 class Animal(SerVivo):
-    def test():
+    def predar(self, plantas, animais):
         return
+
+class Mosquito(Animal):
+    def predar(self, plantas, animais):
+        if self.energia < 270: #verificar se está com fome
+            for planta in plantas:
+                if (abs(self.x - planta.x) <=passo) and (abs(self.y - planta.y) <=passo): #verificar se tem planta perto
+                    plantas.remove(planta) #mata a planta
+                    self.energia += 300 #ganha energia
+                    break
+
+
